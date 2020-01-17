e0691f6d359ec989485e62acfcce13806dd2e425
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-01-17 22:42:04 +0100

    Added <Values/> Back in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, {Component} from "react";
+import React from "react";
 import './App.css';
 
 
 import HangTimer from './components/HangTimer/HangTimer'
 import Header from './components/Header/Header'
-import Countdown from "./components/Countdown";
-import Timer from "./components/Timer"
-import Values from "./components/Values"
+import Values from "./components/Values";
 
 const App = () => (
             <div className="App">
                 <Header title="Hangboard-timer"/>
                 <HangTimer/>
+                <Values/>
             </div>
         );
 
