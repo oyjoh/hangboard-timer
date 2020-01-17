dae903b2db2a0d272cbf4b7e25d97d0514793eb0
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2020-01-17 13:32:14 +0100

    user input stored in state

--- src/components/Values.js
+++ src/components/Values.js
@@ -5,30 +5,42 @@ class Values extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            hang: null,
+            rest1: null,
+            reps1: null,
+            rest2: null,
+            reps2: null
         }
     }
 
-
     handleSubmit = (event) => {
         event.preventDefault()
+        const data = this.state
+        console.log(data)
     }
-    handleInputChange = () => {
 
+    componentDidMount() {
+        this.setState({
+            hang: 10,
+            rest1: 30,
+            reps1: 4,
+            rest2: 120,
+            reps2: 4
+        })
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <p>hang<NumericInput min={0} max={100} value={10} format={secFormat}/></p>
-                    <p>rest<NumericInput min={0} max={100} value={30} format={secFormat}/></p>
-                    <p>reps<NumericInput min={0} max={100} value={4} format={repFormat}/></p>
+                    <p>hang<NumericInput onChange={value => this.setState({hang: value })} name='hang' min={0} max={100} value={this.state.hang} format={secFormat}/></p>
+                    <p>rest<NumericInput onChange={value => this.setState({rest1: value })} name='rest1' min={0} max={100} value={this.state.rest1} format={secFormat}/></p>
+                    <p>reps<NumericInput onChange={value => this.setState({reps1: value })} name='reps1' min={0} max={100} value={this.state.reps1} format={repFormat}/></p>
                     <br></br>
-                    <p>rest<NumericInput min={0} max={300} value={120} format={secFormat}/></p>
-                    <p>reps<NumericInput min={0} max={100} value={4} format={repFormat}/></p>
+                    <p>rest<NumericInput onChange={value => this.setState({rest2: value })} name='rest2' min={0} max={300} value={this.state.rest2} format={secFormat}/></p>
+                    <p>reps<NumericInput onChange={value => this.setState({reps2: value })} name='reps2' min={0} max={100} value={this.state.reps2} format={repFormat}/></p>
                     <p>
-                        <button>Submit</button>
+                        <button>Start timer</button>
                     </p>
                 </form>
             </div>
@@ -42,8 +54,6 @@ class Values extends Component {
             return num + ' reps'
         }
     }
-
-
 }
 
 export default Values
