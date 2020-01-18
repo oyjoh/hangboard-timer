14c7bd9f007365ed6678bce9d0c99f82fc1c7251
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2020-01-18 12:49:22 +0100

    Updates

---
+++ precache-manifest.ce3efaf386a621e592e2fff2e7806be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b385cb31090e8b820de9d6216ae4125a",
+    "url": "/hangboard-timer/index.html"
+  },
+  {
+    "revision": "101f3a3fff137696d54d",
+    "url": "/hangboard-timer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "771fce1ff33701b7ee7a",
+    "url": "/hangboard-timer/static/js/2.21d6e67d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hangboard-timer/static/js/2.21d6e67d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "101f3a3fff137696d54d",
+    "url": "/hangboard-timer/static/js/main.31d6530f.chunk.js"
+  },
+  {
+    "revision": "0193ed538db6908551a3",
+    "url": "/hangboard-timer/static/js/runtime-main.3a69e319.js"
+  },
+  {
+    "revision": "cdaefc7cba142ade59d7db9fd6808c49",
+    "url": "/hangboard-timer/static/media/bell.cdaefc7c.mp3"
+  }
+]);
