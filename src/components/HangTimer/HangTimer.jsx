d71393575334a6fd8f04098c1068588406befb83
Øyvind Johannessen <oyvind.bre.joh@gmail.com>
2020-01-27 20:26:09 +0100

    changed min/max values for input values

--- src/components/HangTimer/HangTimer.jsx
+++ src/components/HangTimer/HangTimer.jsx
@@ -88,8 +88,8 @@ const HangTimer = () => {
                 unit="reps"
                 text="Reps"
                 startVal={state.reps}
-                min={0}
-                max={10}
+                min={1}
+                max={20}
                 incValue={1}/>
                 ,
             key: 2
@@ -114,8 +114,8 @@ const HangTimer = () => {
                 unit="Rounds"
                 text="Rounds"
                 startVal={state.rounds}
-                min={0}
-                max={10}
+                min={1}
+                max={20}
                 incValue={1}/>
                 ,
             key: 4
